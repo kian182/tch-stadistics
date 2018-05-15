b91bd9119de15d97efbb906a1c0f06bbe7f2958b
kruiz <kevin.ruiz@mojix.com>
2018-05-15 10:12:25 -0400

    Adding login form

--- src/components/dash/dash.vue
+++ src/components/dash/dash.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <header style="margin-bottom: 60px;">
+    <header>
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
           <a class="navbar-brand" href="#">Techo Dashboard</a>
--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,41 +1,29 @@
 <template>
-  <div class="container login">
-    <div class="row">
-      <div class="col-lg-6 col-md-8 col-sm-10 col-xs-12 col-centered">
-        <div class="box-login">
-          <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-6">
-              <!--<img src="../../assets/images/starflex_logo_blue_hat.svg" class="img-responsive logo-login" alt=""/>-->
-              <img src="../../assets/images/starflex_logo_blue.svg" class="img-responsive logo-login" alt=""/>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-6">
-              <div class="form-login">
-                <h3 class="margin-top-0 margin-bottom-20 text-center-xs">Licence Generator</h3>
-                <form v-on:submit.prevent="login()">
-                  <div class="form-group">
-                    <input v-model.trim="username" type="text" placeholder="Username" class="form-control"/>
-                  </div>
-                  <div class="form-group form-group-addon">
-                    <input v-model.trim="password" :type="showPassword? 'text':'password'" placeholder="Password" class="form-control form-control-addon"/>
-                    <span type="button" class="input-group-addon" @click="showPassword = !showPassword">
-                      <span class="sprite hand"
-                            :class="{'icon-eye-open': !showPassword , 'icon-eye-closed':showPassword}" title="Show/Hide password"></span>
-                    </span>
-                  </div>
-                  <p class="text-danger" v-if="errors.emptyFields">Enter username and password value</p>
-                  <p class="text-danger" v-if="errors.wrongFields">Wrong username or password</p>
-                  <button value="login" class="btn btn-primary btn-block" type="submit">Login</button>
-                </form>
-              </div>
-            </div>
-          </div>
+  <div class="container">
+    <div class="card card-container">
+      <!-- <img class="profile-img-card" src="//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" alt="" /> -->
+      <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+      <p id="profile-name" class="profile-name-card"></p>
+      <form class="form-signin" v-on:submit.prevent="login()">
+        <span id="reauth-email" class="reauth-email"></span>
+        <input  v-model.trim="username" type="text" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
+        <input v-model.trim="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div id="remember" class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
         </div>
-      </div>
-    </div>
-  </div>
+        <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Sign in</button>
+      </form><!-- /form -->
+      <a href="#" class="forgot-password">
+        Forgot the password?
+      </a>
+    </div><!-- /card-container -->
+  </div><!-- /container -->
 </template>
 
 <script src="./login.js"></script>
 <style lang="scss" src="./login.scss"></style>
+
 
 
